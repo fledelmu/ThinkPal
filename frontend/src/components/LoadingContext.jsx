8382e1b7d8f4929c887959ef8187399c255199d2
Francis <frluemdemu@gmail.com>
2025-06-25 01:27:13 +0800

    Final final fix

--- frontend/src/components/LoadingContext.jsx
+++ frontend/src/components/LoadingContext.jsx
@@ -9,7 +9,7 @@ export const LoadingProvider = ({ children }) => {
   return (
     <LoadingContext.Provider value={{ loading, setLoading, loadingMessage, setLoadingMessage }}>
       {loading && (
-        <div className="fixed inset-0 z-[99999] flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed top-4 right-4 z-[99999]">
           <div className="bg-rule-bg p-4 border-2 border-rule-60 rounded shadow">{loadingMessage}</div>
         </div>
       )}
