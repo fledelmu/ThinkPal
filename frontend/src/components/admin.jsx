2e5bfa07a672e74ae99d6945a943300595f4fb75
Francis <frluemdemu@gmail.com>
2025-10-07 21:42:31 +0800

    Fixing table view

--- frontend/src/components/admin.jsx
+++ frontend/src/components/admin.jsx
@@ -97,7 +97,7 @@ const Admin = () => {
                 <th className="p-2 border border-gray-300">Actions</th>
                 </tr>
             </thead>
-            <tbody className="align-top">
+            <tbody className="align-baseline">
                 {users.map((u) => (
                 <tr key={u.user_id}>
                     <td className="p-2 border border-gray-300">{u.user_id}</td>
