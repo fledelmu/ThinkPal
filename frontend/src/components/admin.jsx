01aa9e17cbf3240cd64c1d06e2055d61399905a8
Francis <frluemdemu@gmail.com>
2025-10-07 21:41:48 +0800

    Fixing ui elements

--- frontend/src/components/admin.jsx
+++ frontend/src/components/admin.jsx
@@ -88,7 +88,7 @@ const Admin = () => {
         </button>
       </div>
 
-      <table className="w-full h-[85%] border-collapse border border-gray-300">
+      <table className="w-full border-collapse border border-gray-300">
             <thead className="bg-gray-200">
                 <tr>
                 <th className="p-2 border border-gray-300">ID</th>
